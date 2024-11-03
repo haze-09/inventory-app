e0198c7b68e7472ca8364fa04a599602528d79d8
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-11-03 15:59:47 +0530

    input delete button added

--- public/js/form.js
+++ public/js/form.js
@@ -1,20 +1,50 @@
 const addArtistButton = document.querySelector("#addArtist");
 const addGenreButton = document.querySelector("#addGenre");
 
-function attachListener(button, name, placeholder, list) {
-    let newInput = document.createElement("input");
-    newInput.setAttribute("type", "text");
-    newInput.setAttribute("name", name);
-    newInput.setAttribute("placeholder", placeholder);
-    newInput.setAttribute("list", list);
-    newInput.required = true;
-    button.insertAdjacentElement("beforebegin", newInput);
+function addInput(button, name, placeholder, list) {
+  let newInput = document.createElement("input");
+  newInput.setAttribute("type", "text");
+  newInput.setAttribute("name", name);
+  newInput.setAttribute("placeholder", placeholder);
+  newInput.setAttribute("list", list);
+  newInput.required = true;
+  button.insertAdjacentElement("beforebegin", newInput);
+}
+
+function deleteInput(container) {
+  const inputs = document.querySelectorAll(`#${container} > input`);
+  const deleteBtn = document.querySelector(`.${container}-delete-btn`);
+
+  if (inputs.length > 1) {
+    if (!deleteBtn) {
+      const btn = document.createElement("button");
+      btn.type = "button";
+      btn.className = `${container}-delete-btn`;
+      btn.innerHTML = '<i class="fa-solid fa-minus"></i>';
+
+      btn.addEventListener("click", () => {
+        let inputs = document.querySelectorAll(`#${container} > input`);
+
+        if(inputs.length == 2){
+            document.querySelector(`.${container}-delete-btn`).remove();
+        }
+        
+        inputs[inputs.length - 1].remove();
+      });
+
+      document.querySelector(`#${container}`).appendChild(btn);
+    }
+  } else if (deleteBtn) {
+    deleteBtn.remove();
+  }
 }
 
 addArtistButton.addEventListener("click", () => {
-    attachListener(addArtistButton, 'artist', 'Artist', 'artistList');
+  addInput(addArtistButton, "artist", "Artist", "artistList");
+  deleteInput("artistInputs");
 });
 
 addGenreButton.addEventListener("click", () => {
-    attachListener(addGenreButton, 'genre', 'Genre', 'genreList');
+  addInput(addGenreButton, "genre", "Genre", "genreList");
+  deleteInput("genreInputs");
 });
